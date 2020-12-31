b50c5b1674557ec8d69a352b12489f642d344076
okutaku <72905878+okutaku@users.noreply.github.com>
2020-12-31 13:31:56 +0900

    追加の部分をcss in jsに変更

--- src/component/inputTodo.jsx
+++ src/component/inputTodo.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+//css in js
+//jsの中にcｓｓを管理すること
+// オブジェクトで管理することになるので書き方に変更あるところに注意が必要
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  borderRadius: "8px",
+  padding: "8px",
+  margin: "8px",
+}
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
     <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
     <button onClick={onClick}>追加</button>
   </div>
@@ -12,4 +24,9 @@ export const InputTodo = (props) => {
 //コンポーネントの手順
 // まずファイルをつくる
 //なんの機能が必要なのか考える
-//その機能（情報）を親（App.js）を参照にpropsでつくる
+//その機能（情報）を親（App.js）を参照にpropsでもとをつくる
+//メリット
+//見た目がよい
+//使いまわしができる
+//CSSの変更 css in js 
+//cssの管理もコンポーネントの中身で行う
